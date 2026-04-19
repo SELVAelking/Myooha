b3759bd916b92b218212bd45f111af25356730b2
SELVAelking <selvaselva111178@gmail.com>
2026-04-19 22:51:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2647,18 +2647,17 @@ def user_public_sms_page():
                         <th>CLI</th>
                         <th>SMS</th>
                         <th>My Payout</th>
-                    </tr>
-                </thead>
+                    </thead>
 <tbody>
-    {rows if rows else f'''
+    {rows if rows else '''
     <tr>
         <td colspan="6" style="text-align: center; padding: 50px;">
             <i class="far fa-comment-dots" style="font-size: 3rem; color: #d9c2f0; margin-bottom: 15px; display: block;"></i>
-                            <span style="color: #8b6baf;">No messages found</span>
-                        </td>
-                    </tr>
-                    '''}
-                </tbody>
+            <span style="color: #8b6baf;">No messages found</span>
+        </td>
+    </tr>
+    '''}
+</tbody>
             </table>
         </div>
         
